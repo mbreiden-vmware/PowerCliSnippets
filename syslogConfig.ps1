0f4cfac78baeefb72ce061d4f3dd84518f4c5dd3
Marcus Breiden <mbreiden@vmware.com>
2016-06-10 10:59:43 +0200

    added syslogConfig, quick script to set default log rotate and syslog host via EsxiCli v2 in Powershell

---
+++ syslogConfig.ps1
@@ -0,0 +1,21 @@
+﻿#requires -Version 1
+#requires -PSSnapin VMware.VimAutomation.Core
+
+# Get all ESXi Hosts, set default rotate value to 8 and syslog host to syslogServer using EsxiCli V2
+
+
+$esxis = Get-VMHost 
+foreach ($esxi in $esxis)
+{
+    $esxcli = Get-EsxCli -VMHost $esxi -V2
+    Write-Host -Object 'Configure Syslog'
+    $esxcli.system.syslog.config.set.invoke(@{
+            defaultrotate = 8
+            loghost       = 'udp://syslogServer:514'
+    })
+    $syslog = Get-VMHostService -VMHost $esxi |
+    Where-Object -FilterScript {
+        $_.key -eq 'vmsyslogd'
+    }
+    Restart-VMHostService -HostService $syslog -Confirm:$false
+}
